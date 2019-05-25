87bc0f230bb5a41fcd01c7e9a5f7332cc1746557
Umar Bilal <umar@dynamiconlinetechnologies.com>
2019-05-25 15:47:31 +0500

    build fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/time-tracker'));
+app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/time-tracker/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
